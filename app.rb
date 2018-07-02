e85329948e1425c083e06479f8a6224878a7b35e
Rebecca Tong <rtong@buffalo.edu>
2018-07-02 18:34:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,9 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-post '/food_form' do
-    erb :food_form
-  end
+# post '/food_form' do
+#     erb :food_form
+#   end
 
   post '/food' do
   "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
